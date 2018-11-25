b2264fe3f3c355935a1b471bf7ea784968b832a6
themagicaldeveloper <45320697+themagicaldeveloper@users.noreply.github.com>
2018-11-24 22:52:44 -0800

    Add files via upload
    Update - Restructured everything and now loops through all champs not just the local player, thanks to RMAN

--- Magical Awareness/Magical Awareness.cpp
+++ Magical Awareness/Magical Awareness.cpp
@@ -2,71 +2,33 @@
 #include "C:/Users/Daniel/Downloads/rift-sdk-master/src/sdkapi.h" 
 #include "Magical Awareness.h"
 
-PSDK_CONTEXT SDK_CONTEXT_GLOBAL;
 SDK_HEALTH Health;
 
-
-struct
-{
-	//The local player obj
+struct {
 	void* Object;
-
-	//
-	// The player's team.
-	//
 	int TeamID;
+} localPlayer;
 
-} _g_LocalPlayer; //This gives us access to the local player object and the local player's teamID
+PLUGIN_SETUP("Magical Awareness", OnLoad);
 
-BOOL WINAPI DllMain //Rift DLL Setup
-(
-	_In_ HINSTANCE hinstDLL,
-	_In_ DWORD fdwReason,
-	_In_ LPVOID lpvReserved
-)
-
-{
-	UNREFERENCED_PARAMETER(hinstDLL);
-
-	if (fdwReason != DLL_PROCESS_ATTACH)
-		return TRUE;
-
-	SDK_EXTRACT_CONTEXT(lpvReserved);
-	if (!SDK_CONTEXT_GLOBAL) {
-		return FALSE;
-	}
-	if (!SDKSTATUS_SUCCESS(SdkNotifyLoadedModule("Simple", SDK_VERSION)))
-	{
-		return FALSE;
-	}
-	//Setup is finished here, Code is added after this
-	SdkRegisterOnLoad //The code here is run as soon as your module is loaded successfuly
-	(
-		[](void* UserData) -> void
-	{
-		UNREFERENCED_PARAMETER(UserData);
-		SdkGetLocalPlayer(&_g_LocalPlayer.Object); //This stores a pointer to the local player object, so that we may use it in other functions.
-		SdkRegisterGameScene(DrawGameScene, NULL); //Callback that will allow us to draw and call code at the start of every frame
-		
-	}, NULL
-	);
-
-	return TRUE;
-}
-
-void __cdecl DrawGameScene //Called by SdkRegisterGameScene, the code in here will be called every frame, useful for drawing visuals
-(
-	_In_ void* UserData
-)
-
-{
+void __cdecl DrawGameScene(_In_ void* UserData) {
 	UNREFERENCED_PARAMETER(UserData);
-	
-	SdkGetUnitHealth(_g_LocalPlayer.Object, &Health); //Grabs the pointer to all health info, useful for getting info about health or shields
-	if (Health.Current==0) // This if statement will print "I am dead!" if the localplayers health is 0, otherwise it will print the localplayers health value
-		SdkUiConsoleWrite("I am dead!");
-	else
-		SdkUiConsoleWrite("My current health is: %f", Health.Current);
+	SdkEnumHeroes(PrintHealth, NULL);
 }
 
+bool __cdecl PrintHealth(void* Object, unsigned int NetworkID, void* UserData) {
 
+	SdkGetUnitHealth(Object, &Health); //Grabs the pointer to all health info, useful for getting info about health or shields
+	if (Health.Current == 0) // This if statement will print "I am dead!" if the localplayers health is 0, otherwise it will print the localplayers health value
+		SdkUiConsoleWrite("A hero is dead!");
+	else
+		SdkUiConsoleWrite("A heroes health is: %f", Health.Current);
+	return true;
+}
+
+void __cdecl OnLoad(void* UserData) {
+	UNREFERENCED_PARAMETER(UserData);
+	SdkGetLocalPlayer(&localPlayer.Object); //This stores a pointer to the local player object, so that we may use it in other functions.
+	SdkRegisterGameScene(DrawGameScene, NULL); //Callback that will allow us to draw and call code at the start of every frame
+	return;
+}
--- Magical Awareness/Magical Awareness.h
+++ Magical Awareness/Magical Awareness.h
@@ -1,6 +1,27 @@
-#pragma once
-void
-__cdecl
-DrawGameScene(
-	_In_ void* UserData
-);
+#define PLUGIN_SETUP(NAME, LOAD_FUNC) \
+void* g_LocalPlayer; \
+PSDK_CONTEXT SDK_CONTEXT_GLOBAL; \
+\
+void __cdecl OnLoad(void* UserData);\
+\
+BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {\
+    CheckSDK(NAME);\
+    \
+    SdkRegisterOnLoad(LOAD_FUNC, NULL);\
+    \
+    return true;\
+}
+
+#define CheckSDK(NAME) \
+UNREFERENCED_PARAMETER(hinstDLL); \
+\
+if (fdwReason != DLL_PROCESS_ATTACH) \
+return FALSE; \
+\
+SDK_EXTRACT_CONTEXT(lpvReserved); \
+if (!SDK_CONTEXT_GLOBAL || !SDKSTATUS_SUCCESS(SdkNotifyLoadedModule(NAME, SDK_VERSION))) { \
+    return FALSE; \
+}
+
+void __cdecl DrawGameScene(void* UserData);
+bool __cdecl PrintHealth(void* Object, unsigned int NetworkID, void* UserData);
